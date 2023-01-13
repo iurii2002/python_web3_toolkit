5d4a48e30af6da1cf8bac104d0363f54d70963af
iurii <iurii.zozulynskyi@gmail.com>
2023-01-13 20:22:17 +0200

    updated provuders

--- client.py
+++ client.py
@@ -1,21 +1,19 @@
 from web3 import Web3
 from methods.get_contract_abi import get_contract_abi
-from providers import HTTP_PROVIDER
+from providers import get_http_provider
 from chain_info import chain_details
+from typing import Optional, Dict, Any, List
 
 
 class Web3Manager:
-
-    w3 = Web3(Web3.HTTPProvider(HTTP_PROVIDER))
-
-    def __init__(self, chain, private_key):
+    def __init__(self, chain: str, private_key: str) -> None:
         """
         :param chain:
         :param private_key:
         """
-        self.chain = chain
+        # self.chain = chain
         self.private_key = private_key
-
+        self.w3 = Web3(Web3.HTTPProvider(chain))
 
     def approve(self, my_address, token_address, spender_address, amount=(2**256 - 1), proxy_contract=None):
         """
@@ -31,15 +29,15 @@ class Web3Manager:
             abi = get_contract_abi(proxy_contract)
         else:
             abi = get_contract_abi(token_address)
-        token_contract = w3.eth.contract(address=token_address, abi=abi)
+        token_contract = self.w3.eth.contract(address=token_address, abi=abi)
         approve_tx = token_contract.functions.approve(spender_address, amount).build_transaction({
             'chainId': chain_details['chainId'],
             'gas': chain_details['gas'],
             'maxFeePerGas': chain_details['maxFeePerGas'],
             'maxPriorityFeePerGas': chain_details['maxPriorityFeePerGas'],
-            'nonce': w3.eth.get_transaction_count(my_address),
+            'nonce': self.w3.eth.get_transaction_count(my_address),
         })
-        signed_txn = w3.eth.account.sign_transaction(approve_tx, private_key=my_private_key)
-        w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+        signed_txn = self.w3.eth.account.sign_transaction(approve_tx, private_key=my_private_key)
+        self.w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 
-        return w3.toHex(w3.keccak(signed_txn.rawTransaction))
+        return self.w3.toHex(self.w3.keccak(signed_txn.rawTransaction))
--- providers.py
+++ providers.py
@@ -1,42 +1,48 @@
 from keys import INFURA_API_KEY
 
-# INFURA
+prividers_dict = {
 
-# ETH L1 Blockchain
-ETH_MAINNET = f'https://mainnet.infura.io/v3/{INFURA_API_KEY}'
-ETH_GOERLI = f'https://goerli.infura.io/v3/{INFURA_API_KEY}'
-ETH_SEPOLIA = f'https://sepolia.infura.io/v3/{INFURA_API_KEY}'
+    # INFURA
 
-# Arbitrum Ethereum L2/Rollup
-ARB_MAINNET = f'https://arbitrum-mainnet.infura.io/v3/{INFURA_API_KEY}'
-ARB_GOERLI = f'https://arbitrum-goerli.infura.io/v3/{INFURA_API_KEY}'
+    # ETH L1 Blockchain
+    'ETH_MAINNET': f'https://mainnet.infura.io/v3/{INFURA_API_KEY}',
+    'ETH_GOERLI': f'https://goerli.infura.io/v3/{INFURA_API_KEY}',
+    'ETH_SEPOLIA': f'https://sepolia.infura.io/v3/{INFURA_API_KEY}',
 
-# Optimism Ethereum L2/Rollup
-OPTIMISM_MAINNET = f'https://optimism-mainnet.infura.io/v3/{INFURA_API_KEY}'
-OPTIMISM_GOERLI = f'https://optimism-goerli.infura.io/v3/{INFURA_API_KEY}'
+    # Arbitrum Ethereum L2/Rollup
+    'ARB_MAINNET': f'https://arbitrum-mainnet.infura.io/v3/{INFURA_API_KEY}',
+    'ARB_GOERLI': f'https://arbitrum-goerli.infura.io/v3/{INFURA_API_KEY}',
 
-# Avalanche C-Chain Ethereum L1
-AVA_MAINNET = f"https://avalanche-mainnet.infura.io/v3/{INFURA_API_KEY}"
-AVA_FUJI = f'https://avalanche-fuji.infura.io/v3/{INFURA_API_KEY}'
+    # Optimism Ethereum L2/Rollup
+    'OPTIMISM_MAINNET': f'https://optimism-mainnet.infura.io/v3/{INFURA_API_KEY}',
+    'OPTIMISM_GOERLI': f'https://optimism-goerli.infura.io/v3/{INFURA_API_KEY}',
 
-# NEAR PoS L1
-NEAR_MAINNET = f'https://near-mainnet.infura.io/v3/{INFURA_API_KEY}'
-NEAR_TESTNET = f'https://near-testnet.infura.io/v3/{INFURA_API_KEY}'
+    # Avalanche C-Chain Ethereum L1
+    'AVA_MAINNET': f'https://avalanche-mainnet.infura.io/v3/{INFURA_API_KEY}',
+    'AVA_FUJI': f'https://avalanche-fuji.infura.io/v3/{INFURA_API_KEY}',
 
-# Starknet Ethereum ZK-Rollup L2
-STARKNET_MAINNET = f'https://starknet-mainnet.infura.io/v3/{INFURA_API_KEY}'
-STARKNET_GOERLI1 =f'https://starknet-goerli.infura.io/v3/{INFURA_API_KEY}'
-STARKNET_GOERLI2 =f'https://starknet-goerli2.infura.io/v3/{INFURA_API_KEY}'
+    # NEAR PoS L1
+    'NEAR_MAINNET': f'https://near-mainnet.infura.io/v3/{INFURA_API_KEY}',
+    'NEAR_TESTNET': f'https://near-testnet.infura.io/v3/{INFURA_API_KEY}',
+
+    # Starknet Ethereum ZK-Rollup L2
+    'STARKNET_MAINNET': f'https://starknet-mainnet.infura.io/v3/{INFURA_API_KEY}',
+    'STARKNET_GOERLI1': f'https://starknet-goerli.infura.io/v3/{INFURA_API_KEY}',
+    'STARKNET_GOERLI2': f'https://starknet-goerli2.infura.io/v3/{INFURA_API_KEY}',
+
+    # Polygon Ethereum NFT Sidechain
+    'POLYGON_MAINNET': f'https://polygon-mainnet.infura.io/v3/{INFURA_API_KEY}',
+    'POLYGON_MUMBAI': f'https://polygon-mumbai.infura.io/v3/{INFURA_API_KEY}',
+
+    # Palm
+    'PALM_MAINNET': f'https://palm-mainnet.infura.io/v3/{INFURA_API_KEY}',
+    'PALM_TESTNET': f'https://palm-testnet.infura.io/v3/{INFURA_API_KEY}',
+
+    # Aurora Ethereum / Near Bridge
+    'AURORA_MAINNET': f'https://aurora-mainnet.infura.io/v3/{INFURA_API_KEY}',
+    'AURORA_TESTNET': f'https://aurora-testnet.infura.io/v3/{INFURA_API_KEY}',
+}
 
 
-# Polygon Ethereum NFT Sidechain
-POLYGON_MAINNET = f'https://polygon-mainnet.infura.io/v3/{INFURA_API_KEY}'
-POLYGON_MUMBAI = f'https://polygon-mumbai.infura.io/v3/{INFURA_API_KEY}'
-
-# Palm
-PALM_MAINNET = f'https://palm-mainnet.infura.io/v3/{INFURA_API_KEY}'
-PALM_TESTNET = f'https://palm-testnet.infura.io/v3/{INFURA_API_KEY}'
-
-# Aurora Ethereum / Near Bridge
-AURORA_MAINNET = f'https://aurora-mainnet.infura.io/v3/{INFURA_API_KEY}'
-AURORA_TESTNET = f'https://aurora-testnet.infura.io/v3/{INFURA_API_KEY}'
+def get_http_provider(chain):
+    return prividers_dict[chain]
